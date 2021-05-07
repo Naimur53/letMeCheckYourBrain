cf580ab1f725e1b7952cb1b5a4f94612fb4267cd
Naimur53 <naimurrhman53@gmail.com>
2021-05-07 22:59:40 +0600

    problem solved

---
+++ elemants/js/main.js
@@ -0,0 +1,24 @@
+//main variable
+var n1=3;
+var n2="5";
+var result;
+//main program
+var sum = n1 + n2;
+    sum = parseInt(sum); //******  
+    sum = sum + "";
+    sum = typeof sum;
+    sum = sum.toUpperCase();
+    sum = sum.indexOf("N");   
+  if(sum > 1){
+   sum = sum*2504 + "";
+   result = sum.split(""); 
+   result = parseInt(result);
+   result = result + "";
+   console.log( "re",result);
+  }
+  else{
+      console.log("not valid");
+  }   
+  console.log( sum);
+  sum = sum.indexOf(result);
+    console.log(sum);
