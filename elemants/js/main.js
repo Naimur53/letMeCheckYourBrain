41a2599ba6b723db1c18ce6e58ac8807f35ed86a
Naimur53 <naimurrhman53@gmail.com>
2021-05-09 02:47:33 +0600

    done shift and unshift

--- elemants/js/main.js
+++ elemants/js/main.js
@@ -28,6 +28,17 @@ var sum = n1 + n2;
     if(sum >0){
         console.log("sum is greater than 0  and the valu is :",  sum);
     }
+    //learning new thing in j
 var friendsAge = [20, 19, 21];
- friendsAge[2] =2; 
+ friendsAge[1] =2; // it well repleace 19 by 2
+ var n=3;
+ friendsAge.push(n);//adding new thing in array 
 console.log(friendsAge);
+console.log(friendsAge.length);//length of arrary (how many elemants array have)
+ 
+var hi = [23,5,56,3]; 
+hi.pop();
+hi.push("sadi")
+hi.unshift("naimur ")//unshift will add element in beginning of array
+hi.shift();
+console.log( hi );
