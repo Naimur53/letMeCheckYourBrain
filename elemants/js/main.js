91a4f0b437e1db34f72fdca8986fad2475ed7d3a
Naimur53 <naimurrhman53@gmail.com>
2021-05-10 01:19:17 +0600

    error

--- elemants/js/main.js
+++ elemants/js/main.js
@@ -40,5 +40,44 @@ var hi = [23,5,56,3];
 hi.pop();
 hi.push("sadi")
 hi.unshift("naimur ")//unshift will add element in beginning of array
-hi.shift();
+hi.shift();//it will remove element in the beginning of array
 console.log( hi );
+/*
+push adding last 
+pop remove last 
+unshift adding first 
+shift removing first
+*/ 
+hi=hi.slice(2);//it wil delete first 0 1 and start showing at  2 to end ;
+console.log(hi);
+var song = 0;
+while(song <=10){
+    console.log("said",song); 
+    song++;
+}
+
+var naimur = [2,4,5,6];
+var l=naimur.length;
+var r = 0;
+var c ;
+while(r<l){ 
+    c = naimur[r]; 
+    console.log(c);
+    r++;
+}
+for (var i=0; i<l; i++)
+{
+    console.log(naimur[i]);
+}
+var avi = [3334,4343,32345,432];
+var x = 0;  
+var y ;
+while(4000>x){
+    y = avi.indexOf(x);
+    console.log("y",y);
+    if(y>0){
+        console.log(x);
+    }
+    x++;
+
+}
